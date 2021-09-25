165cfa312a32a9fb8b5e4dfcb4a2473791fa6dce
Hammurabi <koberstine@hotmail.com>
2021-09-25 15:41:00 -0400

    updated project component

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,51 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Project() {
-  const [projects] = [
+  const [projects] = useState([
     {
-      name: 'Budget Tracker',
-      deployed: 'https://limitless-inlet-80650.herokuapp.com/',
-      github: 'https://github.com/koberstine/budget-tracker',
-      image: '',
+      name: 'Redux Store',
+      deployed: 'https://murmuring-brushlands-08289.herokuapp.com/',
+      github: 'https://github.com/koberstine/redux-store/',
+      image: '../../assets/images/image1.jpg',
       screenshot: '../../assets/images/screenshot1.jpg'
     },
     {
-      name: 'Note Taker',
-      deployed: 'https://secure-tor-32394.herokuapp.com/',
-      github: 'https://github.com/koberstine/note-taker',
-      image: '',
+      name: 'Book Wishlist',
+      deployed: 'https://pure-gorge-10833.herokuapp.com/',
+      github: 'https://github.com/koberstine/book-wishlist/',
+      image: '../../assets/images/image2.jpg',
       screenshot: '../../assets/images/screenshot2.jpg'
     },
     {
-      name: 'Weather Dashboard',
-      deployed: 'https://koberstine.github.io/weather-dashboard/',
-      github: 'github.com/koberstine/weather-dashboard',
-      image: '',
+      name: 'Budget Tracker',
+      deployed: 'https://limitless-inlet-80650.herokuapp.com/',
+      github: 'https://github.com/koberstine/budget-tracker/',
+      image: '../../assets/images/image3.jpg',
       screenshot: '../../assets/images/screenshot3.jpg'
     },
     {
       name: 'Share Thought',
       deployed: 'https://vast-shore-81150.herokuapp.com/',
-      github: 'https://github.com/koberstine/share-thought',
-      image: '',
+      github: 'https://github.com/koberstine/share-thought/',
+      image: '../../assets/images/image4.jpg',
       screenshot: '../../assets/images/screenshot4.jpg'
     },
     {
-      name: 'Work Day Scheduler',
-      deployed: 'https://koberstine.github.io/work-day-scheduler/',
-      github: 'github.com/koberstine/work-day-scheduler',
-      image: '',
+      name: 'Note Taker',
+      deployed: 'https://secure-tor-32394.herokuapp.com/',
+      github: 'https://github.com/koberstine/note-taker/',
+      image: '../../assets/images/image5.jpg',
       screenshot: '../../assets/images/screenshot5.jpg'
     },
     {
-      name: 'Password Generator',
-      deployed: 'https://koberstine.github.io/password-generator/',
-      github: 'github.com/koberstine/password-generator',
-      image: '',
+      name: 'Weather Dashboard',
+      deployed: 'https://koberstine.github.io/weather-dashboard/',
+      github: 'https://github.com/koberstine/weather-dashboard/',
+      image: '../../assets/images/image5.jpg',
       screenshot: '../../assets/images/screenshot6.jpg'
     }
-  ]
+  ]);
 
-}
+  return (
+    <div className="flex-row">
+      {projects.map((project, i) => (
+          <img
+          src= {project.image}
+          alt= {project.name}
+          className="img-thumbnail mx-1"
+        />
+      ))}   
+    </div>
+  )
+};
 
 export default Project;
